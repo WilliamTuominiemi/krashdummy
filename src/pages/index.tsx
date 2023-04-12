80cfa4eb6a7db13d9af91a55f1a58983038243a0
williamtuominiemi <william.m.tuominiemi@gmail.com>
2023-04-12 23:37:39 +0300

    connect to server

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from 'react'
+
 export default function Home() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch('http://localhost:3000/api/hello')
+            const json = await result.json()
+            setData(json)
+            console.log(json)
+        }
+        fetchData()
+    }, [])
+
     return (
         <div>
             <h1 className="text-lg p-5">Krash</h1>
             <button>PLAY</button>
+            <p>{data.message}</p>
         </div>
     )
 }
