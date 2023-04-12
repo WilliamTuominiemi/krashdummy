99c6182f20da385c062e65a0301e6a54175adede
williamtuominiemi <william.m.tuominiemi@gmail.com>
2023-04-12 23:14:25 +0300

    add tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div>
-            <h1>Krash</h1>
+            <h1 className="text-lg p-5">Krash</h1>
             <button>PLAY</button>
         </div>
     )
