a75349bf041ae518666a607aa0ad8ccb9905185f
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2023-04-18 16:47:59 +0300

    draw after mouse on canvas

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -6,37 +6,51 @@ const socket = io('http://localhost:8080')
 
 const Canvas = (props: any) => {
     const canvasRef = useRef<HTMLCanvasElement>(null)
+    let mouseX: number
+    let mouseY: number
 
     useEffect(() => {
+        
+        
         const canvas = canvasRef.current
         if (canvas) {
             const context = canvas.getContext('2d')
+            const canvasRect = canvas.getBoundingClientRect();
+
             if (context) {
-                //Our first draw
+                canvas.height = window.innerHeight * (2 /3) ;
+                canvas.width = window.innerWidth / 2;
+                const width = canvas.width;
+                const height = canvas.height;
+
+                console.log(`[${width}, ${height}]`)
+
                 context.fillStyle = '#FFFFFF'
                 context.fillRect(0, 0, context.canvas.width, context.canvas.height)
 
-                const shortcut = (e: KeyboardEvent) => {
-                    if (e.key === 'w') {
-                        context.fillStyle = '#000000'
-                        context.fillRect(1, 1, 10, 10)
-                        socket.emit('place', 'test')
-                    }
+                 canvas.addEventListener('mousemove', (e) => {
+                    mouseX = e.clientX - canvasRect.left;
+                    mouseY = e.clientY - canvasRect.top;
 
-                    if (e.key === 's') {
-                        console.log('KEY Z')
-                        context.fillStyle = '#000000'
-                        context.fillRect(1, 25, 10, 10)
-                    }
-                }
+                    console.log(`client: ${e.clientX}, ${e.clientY}`)
+                    console.log(`rect: ${canvasRect.left}, ${canvasRect.top}`)
 
-                document.addEventListener('keydown', shortcut)
-                return () => document.removeEventListener('keydown', shortcut)
+                    console.log(`[${mouseX}, ${mouseY}]`)
+
+                    context.beginPath();
+                    context.arc(mouseX,mouseY, 1, 0, Math.PI*2, false);
+                    context.fillStyle = 'red';
+                    context.fill();
+                })              
             }
         }
     }, [])
 
-    return <canvas ref={canvasRef} {...props} />
+    return (
+        <div className="">
+            <canvas ref={canvasRef} {...props} />
+        </div>
+    )
 }
 
 export default Canvas
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -5,12 +5,9 @@ export default function Home() {
     useEffect(() => {}, [])
 
     return (
-        <div>
-            <h1 className="text-lg p-5">Krash</h1>
-            <button>PLAY</button>
-            <div className="outline">
-                <Canvas />
-            </div>
-        </div>
+        <>
+            <h1 className="text-lg p-5">KrashDummy</h1>
+            <Canvas />
+        </>
     )
 }
