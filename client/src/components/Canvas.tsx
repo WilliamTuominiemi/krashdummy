b5d74108a9a61629456d2f44476e3eaefa7c6130
williamtuominiemi <william.m.tuominiemi@gmail.com>
2023-04-21 19:49:33 +0300

    ball follow mouse

--- client/src/components/Canvas.tsx
+++ client/src/components/Canvas.tsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect, useCallback } from 'react'
+import React, { useRef, useEffect, useCallback, useState } from 'react'
+import { isConditionalExpression } from 'typescript'
 
 interface Props {
     width: number
@@ -8,19 +9,28 @@ interface Props {
 const Canvas: React.FC<Props> = (props) => {
     const canvasRef = useRef<HTMLCanvasElement>(null)
 
-    const draw = useCallback((ctx: CanvasRenderingContext2D, frameCount: number) => {
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        ctx.fillStyle = '#140a14'
-        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
-        ctx.fillStyle = '#000000'
-        ctx.beginPath()
-        ctx.arc(50, 50, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI)
-        ctx.fill()
-    }, [])
+    const [mouseX, setMouseX] = useState(0)
+    const [mouseY, setMouseY] = useState(0)
+
+    const draw = useCallback(
+        (ctx: CanvasRenderingContext2D, frameCount: number) => {
+            console.log('change')
+            console.log(`[${mouseX}; ${mouseY}]`)
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+            ctx.fillStyle = '#140a14'
+            ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+            ctx.fillStyle = '#000000'
+            ctx.beginPath()
+            ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI)
+            ctx.fill()
+        },
+        [mouseX, mouseY]
+    )
 
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas!.getContext('2d')
+
         let frameCount = 0
         let animationFrameId: number
 
@@ -37,7 +47,13 @@ const Canvas: React.FC<Props> = (props) => {
         }
     }, [draw])
 
-    return <canvas ref={canvasRef} {...props} />
+    const followMouse = (e: any) => {
+        setMouseX(e.clientX - e.target.offsetLeft)
+        setMouseY(e.clientY - e.target.offsetTop)
+
+        console.log(`[${mouseX}; ${mouseY}]`)
+    }
+    return <canvas onMouseMove={(e) => followMouse(e)} ref={canvasRef} {...props} />
 }
 
 export default Canvas
