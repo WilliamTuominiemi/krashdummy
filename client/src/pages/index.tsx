6c93a1a1ef766a17ee9aea022427a6874b68c537
williamtuominiemi <william.m.tuominiemi@gmail.com>
2023-05-09 23:25:04 +0300

    ui

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,8 +4,11 @@ import Link from 'next/link'
 
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-screen w-screen">
-            <Link href="/play">PLAY</Link>
+        <div className="flex flex-col justify-center items-center h-screen w-screen">
+            <Link href="/play" className="text-blue-500 hover:text-blue-800 font-bold text-2xl">
+                PLAY
+            </Link>
+            <p className="text-blue-600">Queue: x</p>
         </div>
     )
 }
